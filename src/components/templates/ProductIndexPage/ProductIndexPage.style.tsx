9f380739ac6eb71b40f230f06200fc57d8a60509
William Morales <william.rolando88@hotmail.com>
2023-10-16 19:46:02 -0500

    Add search field placeholder

---
+++ src/components/templates/ProductIndexPage/ProductIndexPage.style.tsx
@@ -0,0 +1,8 @@
+import { CSSProperties } from "react";
+
+export const style: CSSProperties = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  alignItems: "start",
+};
--- src/components/templates/ProductIndexPage/index.tsx
+++ src/components/templates/ProductIndexPage/index.tsx
@@ -1,8 +1,14 @@
 import { FC } from "react";
 import ProductsTable from "../../organisms/ProductsTable";
+import { style } from "./ProductIndexPage.style";
 
 const ProductIndexPage: FC = () => {
-  return <ProductsTable />;
+  return (
+    <div style={style}>
+      <input type="text" placeholder="This wanna be search field..." />
+      <ProductsTable />
+    </div>
+  );
 };
 
 export default ProductIndexPage;
